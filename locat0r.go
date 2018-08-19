da300a5358f2b4a6920f810f6fd056ac9cf1ee2a
Florian Heubeck <flo@rohrpostix.net>
2018-08-19 18:04:37 +0200

    Testing with influx

--- locat0r.go
+++ locat0r.go
@@ -12,11 +12,19 @@ import (
 	"strings"
 	"time"
 
+	influx "github.com/influxdata/influxdb/client/v2"
 	"github.com/labstack/echo"
-	gpx "github.com/twpayne/go-gpx"
+	"github.com/twpayne/go-gpx"
 )
 
 var healthTmpl = template.New("health")
+
+const (
+	addr   = "http://localhost:8086"
+	db     = "locat0r"
+	user   = "admin"
+	passwd = "geheim"
+)
 
 type position struct {
 	Lon  float64   `json:"lon"`
@@ -37,11 +45,22 @@ func main() {
 	<code>
 		Protocol: {{.Proto}}<br>
 		Host: {{.Host}}<br>
-		Remote Address: {{.RemoteAddr}}<br>
+		Remote address: {{.Remoteaddr}}<br>
 		Method: {{.Method}}<br>
 		Path: {{.URL.Path}}<br>
 	</code>
 	`)
+
+	con, err := connectInflux()
+	if err != nil {
+		log.Fatalf("Failed to connect to %v: %v", addr, err)
+	}
+	q := influx.Query{Command: "create database " + db, Database: db}
+	_, err = con.Query(q)
+	if err != nil {
+		log.Fatalf("Could not create database %v: %v", db, err)
+	}
+	con.Close()
 
 	e := echo.New()
 	e.Static("/", "static")
@@ -61,7 +80,8 @@ func health(c echo.Context) error {
 
 func getPosition(c echo.Context) error {
 
-	position := position{Lon: 11.2914025, Lat: 48.6810944, Time: time.Now()}
+	waypoints := queryWaypoints(1)
+	position := position{Lon: waypoints[0].Lon, Lat: waypoints[0].Lat, Time: time.Now()}
 
 	jsonPos, err := json.Marshal(position)
 	if err != nil {
@@ -73,23 +93,27 @@ func getPosition(c echo.Context) error {
 
 func getTrack(c echo.Context) error {
 
-	var waypoints []*gpx.WptType
+	waypoints := queryWaypoints(100)
 
-	waypoints = append(waypoints, &gpx.WptType{
-		Lat: 48.6855344, Lon: 11.2511425,
-	})
-	waypoints = append(waypoints, &gpx.WptType{
-		Lat: 48.6844522, Lon: 11.2312325,
-	})
-	waypoints = append(waypoints, &gpx.WptType{
-		Lat: 48.6833643, Lon: 11.2213225,
-	})
-	waypoints = append(waypoints, &gpx.WptType{
-		Lat: 48.6821744, Lon: 11.2714125,
-	})
-	waypoints = append(waypoints, &gpx.WptType{
-		Lat: 48.6810944, Lon: 11.2914025,
-	})
+	/*
+		var waypoints []*gpx.WptType
+
+		waypoints = append(waypoints, &gpx.WptType{
+			Lat: 48.6855344, Lon: 11.2511425,
+		})
+		waypoints = append(waypoints, &gpx.WptType{
+			Lat: 48.6844522, Lon: 11.2312325,
+		})
+		waypoints = append(waypoints, &gpx.WptType{
+			Lat: 48.6833643, Lon: 11.2213225,
+		})
+		waypoints = append(waypoints, &gpx.WptType{
+			Lat: 48.6821744, Lon: 11.2714125,
+		})
+		waypoints = append(waypoints, &gpx.WptType{
+			Lat: 48.6810944, Lon: 11.2914025,
+		})
+	*/
 
 	g := &gpx.GPX{
 		Version: "1.0",
@@ -119,6 +143,7 @@ func postPosition(c echo.Context) error {
 func processNewPosition(mapMyTrack string) {
 	if position, err := parseMapMyTrack(mapMyTrack); err == nil {
 		log.Printf("Got position: %s", position)
+		savePosition(&position)
 	} else {
 		log.Printf("Failed to parse position: %v", err)
 	}
@@ -143,4 +168,68 @@ func parseMapMyTrack(data string) (position, error) {
 	}
 
 	return position{Lat: points[0], Lon: points[1], Time: time.Now()}, nil
+}
+
+func savePosition(p *position) {
+	c, err := connectInflux()
+	if err != nil {
+		log.Printf("Error connecting influx at %v: %v", addr, err)
+		return
+	}
+	defer c.Close()
+
+	bp, err := influx.NewBatchPoints(influx.BatchPointsConfig{
+		Database:  db,
+		Precision: "us"})
+
+	tags := map[string]string{"position": "gps-position"}
+	fields := map[string]interface{}{
+		"lat": p.Lat,
+		"lon": p.Lon}
+
+	pt, err := influx.NewPoint("position", tags, fields, p.Time)
+	if err != nil {
+		log.Printf("Error creating new point: %v", err)
+	}
+	bp.AddPoint(pt)
+
+	if err = c.Write(bp); err != nil {
+		log.Printf("Error writing position: %v", err)
+	}
+}
+
+func connectInflux() (influx.Client, error) {
+	return influx.NewHTTPClient(influx.HTTPConfig{
+		Addr:     addr,
+		Username: user,
+		Password: passwd})
+}
+
+func queryWaypoints(n int) []*gpx.WptType {
+	c, err := connectInflux()
+	if err != nil {
+		log.Printf("Failed to connect influx for querying: %v", err)
+	}
+	defer c.Close()
+
+	var cmd = fmt.Sprintf(`SELECT "lat", "lon" FROM "locat0r"."autogen"."position" order by time desc limit %d`, n)
+	q := influx.Query{Command: cmd, Database: db}
+
+	res, err := c.Query(q)
+	if err != nil {
+		log.Printf("Failed to query %v: %v", cmd, err)
+	}
+
+	var waypoints []*gpx.WptType
+
+	for _, r := range res.Results[0].Series[0].Values {
+		// time := r[0].(time.Time)
+		lat, _ := r[1].(json.Number).Float64()
+		lon, _ := r[2].(json.Number).Float64()
+		waypoints = append(waypoints, &gpx.WptType{
+			Lat: lat, Lon: lon})
+		// log.Printf("i: %v, time: %v, lat: %v, lon: %v", i, r[0], r[1], r[2])
+	}
+
+	return waypoints
 }
